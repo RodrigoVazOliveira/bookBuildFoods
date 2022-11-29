5e578905a48a76467bebb1d2359f38374c0ceeb2
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-11-29 19:46:46 -0300

    Criar modelo de receita

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    ingredients = models.TextField()
+    mode_prepare = models.TextField()
+    time_prepare = models.IntegerField()
+    income = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    date_time_created = models.DateTimeField(default=datetime.now(), blank=True)
+
+
+
+
