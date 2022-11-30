708573baca4e02d2751848802823753cc6b03ed5
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-11-30 00:28:40 -0300

    Adicionar paginacao, buscar por campo pelo nome, listagem com paginacao

--- recipe/admin.py
+++ recipe/admin.py
@@ -5,5 +5,8 @@ from recipe.models import Recipe
 class ListRecipes(admin.ModelAdmin):
     list_display = ('id', 'name', 'time_prepare')
     list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_filter = ('category',)
+    list_per_page = 2
 
 admin.site.register(Recipe, ListRecipes)
