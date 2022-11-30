922f3011608398639d24be63f34a51206ba877b8
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-11-30 00:23:11 -0300

    Adicionar labels na tabela do admin e links nas colunas

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from recipe.models import Recipe
 
-admin.site.register(Recipe)
+
+class ListRecipes(admin.ModelAdmin):
+    list_display = ('id', 'name', 'time_prepare')
+    list_display_links = ('id', 'name')
+
+admin.site.register(Recipe, ListRecipes)
