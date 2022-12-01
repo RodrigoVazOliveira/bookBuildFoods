0badc669b16b36d751d5c12316e873be524acc28
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-12-01 04:00:22 -0300

    Criar regra para busca no view

--- recipe/views.py
+++ recipe/views.py
@@ -19,4 +19,12 @@ def receita(request, recipe_id):
 
 
 def search(request):
+    recipes = Recipe.objects.order_by('-date_time_created').filter(published=True)
+    if 'search' in request.GET:
+        name_recipe = request.GET['search']
+        if name_recipe:
+            recipes = recipes.filter(name__icontains=name_recipe)
+        datas = {'recipes': recipes}
+        return render(request, 'search.html', datas)
+
     return render(request, 'search.html')
