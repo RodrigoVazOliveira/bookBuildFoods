a036ff2970503812ee18b350064eb23fb855abe5
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-12-01 04:36:27 -0300

    Criar funcoes

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def register(request):
+    pass
+
+def login(request):
+    pass
+
+def dashboard(request):
+    pass
+
+def logout(request):
+    pass
